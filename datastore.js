4b5dc5863713d34e837be49caf3014feed8641c4
Jacob Coons <jacobtcoons@gmail.com>
2018-07-01 12:14:31 -0700

    initial commit

---
+++ datastore.js
@@ -0,0 +1,110 @@
+const questionSet = [
+  { 
+    number: 1,
+    text: `Who is the protagonist in the original Star Wars trilogy?`,
+    ans1: `Leia Organa`,
+    ans2: `Luke Skywalker`, 
+    ans3: `Han Solo`, 
+    ans4: `R2-D2`
+  }, 
+
+  {
+    number: 2,
+    text: `In "Star Wars: A New Hope", what was the name of the first planet to be destroyed by the Death Star in a demonstration of Imperial power?`,
+    ans1: `Kashyyyk`, 
+    ans2: `Dantooine`, 
+    ans3: `Hoth`, 
+    ans4: `Alderaan`
+  }, 
+
+  {
+    number: 3,
+    text: `Who commissioned the building of a clone army for the Galactic Republic from the Kaminoans?`,
+    ans1: `Jedi Master Plo Koon`, 
+    ans2: `Jedi Master Qui-Gon Jinn`, 
+    ans3: `Jedi Master Sifo-Dyas`, 
+    ans4: `Jedi Master Ki-Adi-Mundi`
+  }, 
+  
+  {
+    number: 4, 
+    text: `Who trained Jedi Master Qui-Gon-Jinn?`,
+    ans1: `Count Dooku`, 
+    ans2: `Yoda`, 
+    ans3: `Mace Windu`, 
+    ans4: `Obi-Wan Kenobi`
+  }, 
+  
+  {
+    number: 5,
+    text: `What is the name of the Supreme Leader of the First Order?`,
+    ans1: `Snook`, 
+    ans2: `Snoke`, 
+    ans3: `Snork`, 
+    ans4: `Snake`
+  }, 
+  
+  {
+    number: 6,
+    text: `After the fall of the Galactic Empire and the might of the Sith, what group of Dark-Side Force-users replaced them in the era of the First Order?`,
+    ans1: `The Dark Knights`, 
+    ans2: `The Knights of Rey`, 
+    ans3: `The Knights of Ren`, 
+    ans4: `The Knights of Kylo`
+  }, 
+  
+  {
+    number: 7,
+    text: `What is the name of the most famous and talented pilot in the Resistance?`,
+    ans1: `Finn`, 
+    ans2: `Rey`, 
+    ans3: `Maz`, 
+    ans4: `Poe`
+  },
+  
+  {
+    number: 8,
+    text: `After the dissolving of the Galactic Republic, who coined the phrase "So this is how liberty dies... with thunderous applause."?`,
+    ans1: `Padmé Amidala`, 
+    ans2: `Anakin Skywalker`, 
+    ans3: `Obi-Wan Kenobi`, 
+    ans4: `Chancellor Palpetine`
+  },
+  
+  {
+    number: 9,
+    text: `What is a Kyber Crystal?`,
+    ans1: `A power source`, 
+    ans2: `A nearly-sentient being`, 
+    ans3: `A bridge between the Force and the physical realm`, 
+    ans4: `All of the above`
+  },
+  
+  {
+    number: 10,
+    text: `The Millennium Falcon was able to complete the Kessel Run in how many parsecs?`,
+    ans1: `9`, 
+    ans2: `14`, 
+    ans3: `12`, 
+    ans4: `4`
+  }
+];
+
+const ANSWERS = [ 
+  `Luke Skywalker`, 
+  `Alderaan`, 
+  `Jedi Master Sifo-Dyas`, 
+  `Count Dooku`, 
+  `Snoke`, 
+  `The Knights of Ren`, 
+  `Poe`, 
+  `Padmé Amidala`, 
+  `All of the above`, 
+  `12`
+];
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,162 @@
+let questionNum = 1;
+
+let correctAnswers = 0;
+
+function questionTemplate(correctAnswers, question, questionsAnswered) {
+  return `
+    <main role="main" id="question-format">
+     <form>
+        <h3>${question.text}</h3>
+        <fieldset>
+          <legend>Question 1</legend>
+          <label>
+            <input type="radio" name="option">
+            <span>${question.ans1}</span>
+          </label>
+          <label>
+            <input type="radio" name="option">
+            <span>${question.ans2}</span>
+          </label>
+          <label>
+            <input type="radio" name="option">
+            <span>${question.ans3}</span>
+          </label>
+          <label>
+            <input type="radio" name="option">
+            <span>${question.ans4}</span>
+          </label>
+        </fieldset>
+        <button type="button" id="js-submit-button">SUBMIT</button>
+      </form>
+
+      <div class="tracker">
+        <span id="question-counter">Question ${question.number}/10</span>
+        <span id="score-counter">Score: ${correctAnswers}/${questionsAnswered}</span>
+      </div>
+    </section>
+  `;
+}
+
+function handleStartButton() {
+  $('#js-start-button').click(function(event) {
+    nextQuestion();
+  });
+}
+
+function handleSubmitButton() {
+  $('.container').on('click', '#js-submit-button', function(event) {
+    event.preventDefault()
+
+    const answer = $('input:checked').siblings('span');
+
+    const userIsCorrect = checkUserAnswer(answer);
+    if(userIsCorrect) {
+      generateCorrectFeedback();
+    } else {
+      generateIncorrectFeedback();
+    }
+  });
+}
+
+function handleNextButton() {
+  $('.container').on('click', '#js-next-button', function(event) {
+
+    if(questionNum === 10) {
+      createResultsPage(correctAnswers);
+    } else {
+      iterateQuestion();
+      nextQuestion();
+  }
+  });
+}
+
+function handleRestartButton() {
+  $('.container').on('click', '#js-restart-button', function(event) {
+
+    questionNum = 1;
+
+    correctAnswers = 0;
+
+    nextQuestion();
+  });
+}
+
+function nextQuestion() {
+
+  const question = questionSet[questionNum - 1];
+
+  const questionsAnswered = questionNum - 1;
+
+  $('.container').html(questionTemplate(correctAnswers, question, questionsAnswered));
+}
+
+function checkUserAnswer(answer) {
+  if(answer.text() === ANSWERS[questionNum - 1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function generateCorrectFeedback() {
+  $('.container').html(correctFeedback);
+  iterateCorrectAnswers();
+}
+
+const correctFeedback = `
+  <section role="region" class="correct-feedback-page">
+    <div style="width:100%;height:100%;">
+      <h2>Correct! BB-8 approves.</h2>
+      <iframe src="https://giphy.com/embed/3o7abB06u9bNzA8lu8" width="75%" height="75%" frameBorder="0" class="correct-gif">
+      </iframe>
+    </div>
+    <div>
+      <button id="js-next-button" type="button">NEXT QUESTION</button>
+    </div
+  </section>
+`;
+
+function generateIncorrectFeedback() {
+  $('.container').html(incorrectFeedbackTemplate(questionNum));
+}
+
+function incorrectFeedbackTemplate(questionNum) {
+  return `
+    <section role="region" class="incorrect-feedback-page">
+      <div style="width:100%;height:100%px;">
+        <h2>Oh, no! The correct answer was ${ANSWERS[questionNum - 1]}. Looks like The Empire won this round...</h2>
+        <iframe src="https://giphy.com/embed/AcPT09jtaLbNK" width="75%" height="75%" frameBorder="0" class="incorrect-gif">
+        </iframe>
+      </div>
+      <div>
+        <button id="js-next-button" type="button">NEXT QUESTION</button>
+      </div>
+    </section>
+`;
+}
+
+function iterateQuestion() {
+  questionNum++;
+}
+
+function iterateCorrectAnswers() {
+  correctAnswers++;
+}
+
+function createResultsPage(correctAnswers) {
+  $('.container').html(`
+    <section role="region" class="end-page">
+      <h2>Final Score: ${correctAnswers}/10</h2>
+      <button id="js-restart-button" type="button">TRY AGAIN</button>
+    </section>
+  `);
+}
+
+function handleButtons() {
+  handleStartButton();
+  handleSubmitButton();
+  handleNextButton();
+  handleRestartButton();
+}
+
+handleButtons();
